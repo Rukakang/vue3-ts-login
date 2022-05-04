20428ee01a28b2f9225a0cac38d0b703583e6d67
903958734@qq.com <903958734@qq.com>
2022-05-04 12:18:38 +0800

    login page

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,11 +1,72 @@
 <template>
   <div class="login_Container">
-    <el-button>test</el-button>
+    <el-form
+        ref="ruleFormRef"
+        :model="ruleForm"
+        :rules="rules"
+        label-width="120px"
+        class="demo-ruleForm"
+    >
+        <el-form-item label="用户名" prop="userName">
+            <el-input v-model="ruleForm.userName" type="text"  autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="密码" prop="pwd">
+            <el-input v-model="ruleForm.pwd" type="password" autocomplete="off" />
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary" @click="loginFn(ruleFormRef)">登录</el-button>
+            <el-button @click="resetForm(ruleFormRef)">重置</el-button>
+        </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { reactive, ref } from 'vue'
+import type { FormInstance } from 'element-plus'
 
+const ruleFormRef = ref<FormInstance>()
+const validateUser = (rule: any, value: any, callback: any) => {
+  if (value === '') {
+    callback(new Error('请输入用户名'))
+  } else {
+    callback()
+  }
+}
+const validatePwd = (rule: any, value: any, callback: any) => {
+  if (value === '') {
+    callback(new Error('请输入密码'))
+  }else {
+    callback()
+  }
+}
+
+const ruleForm = reactive({
+  userName: '',
+  pwd: '',
+})
+
+const rules = reactive({
+  userName: [{ validator: validateUser, trigger: 'blur' }],
+  pwd: [{ validator: validatePwd, trigger: 'blur' }],
+})
+
+const loginFn = (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  formEl.validate((valid) => {
+    if (valid) {
+      console.log('submit!')
+    } else {
+      console.log('error submit!')
+      return false
+    }
+  })
+}
+
+const resetForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  formEl.resetFields()
+}
 </script>
 
 <style lang="less" scoped>
